ef8545e59127a83f3f6cb16fa02fb5a2eb93e6eb
Tony Gaudio <wireman131@chartermi.net>
2011-01-22 17:44:29 -0500

    added documentation to index.php

--- index.php
+++ index.php
@@ -1,1 +1,34 @@
 <?php
+/**
+ * Anthony Gaudio
+ * ANM293 - Advanced PHP
+ * Project 01
+ * Gitting to know Git
+ */
+ob_start();
+/*
+ * Start output buffering
+ * This means to 'capture' everything in the buffer and don't immediately
+ * send it to the browser.
+ */
+
+echo 'My First Project with Output Buffering';
+/*
+ * Sample echo statement with text intended to be displayed in the browser
+ */
+$v = ob_get_contents();
+/*
+ * assign content of buffer to the variable $v with ob_get_contents();
+ */
+ob_end_clean();
+/*
+ * clean, or empty the buffer
+ * 
+ */
+echo $v;
+/*
+ * display the contents on varible $v which just received the contents of the
+ * buffer.
+ * 
+ */
+?>
